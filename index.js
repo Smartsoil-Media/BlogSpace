4510e9297d5622a95460ed4ab0686c070d38f19b
GunClayton <114902151+Smartsoil-Media@users.noreply.github.com>
2023-11-09 17:17:56 +0800

    commit

--- index.js
+++ index.js
@@ -11,7 +11,33 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         `
         }
         document.getElementById("blog-list").innerHTML = html
-        console.log(html)
+
       
 
     })
+
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+    .then(res => res.json())
+    .then(post =>      
+        document.getElementById("blog-list").innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        <hr />
+        ${document.getElementById("blog-list").innerHTML}
+    `
+)
+})
